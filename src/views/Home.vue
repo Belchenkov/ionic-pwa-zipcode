d5de075341f9b7de3a91bec54109d064f7d9e4a9
Алексей Бельченков <u608110@gmail.com>
2019-05-27 18:46:07 +0400

    Cleate ZipSearch Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,31 @@
         <ion-title>ZipInfo</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">My App</ion-content>
+    <ion-content class="ion-padding">
+      <zip-search
+        @get-zip="getZipInfo"
+      />
+    </ion-content>
   </div>
 </template>
 
 <script>
+import ZipSearch from "../components/ZipSearch";
+
 export default {
   name: 'home',
   components: {
+    ZipSearch
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    async getZipInfo(zip) {
+      console.log(zip);
+    }
   }
 }
 </script>
